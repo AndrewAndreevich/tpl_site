8447e3cc2ca2872d37e104bc56af3c0670be0db3
Andrei <andre3vich@gmail.com>
2025-08-21 15:34:46 +0400

    add style 7

--- script.js
+++ script.js
@@ -109,12 +109,12 @@ function createPrimitive() {
 var options = {
   perlin: {
     vel: 0.002,
-    speed: 0.00050,
+    speed: 0.000050,
     perlins: 1.0,
-    decay: 0.10,
-    complex: 0.30,
+    decay: 0.03,
+    complex: 0.17,
     waves: 20.0,
-    eqcolor: 11.0,
+    eqcolor: 15.0,
     fragment: true,
     redhell: true
   },
